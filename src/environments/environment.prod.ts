fd435aadbf97ad4544d431809eba98d2ad6fa0d6
Simon HEGE <simonhege@gmail.com>
2020-02-15 15:04:25 +0100

    Update api to xdbsoft

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   authConfig: {
-      issuer: 'https://login.okiapps.com/',
+      issuer: 'https://login.xdbsoft.com/',
       redirectUri: window.location.origin + '/index.html',
       clientId: 'notes',
       scope: 'openid profile email',
       strictDiscoveryDocumentValidation: false
   },
   apiConfig: {
-    url: 'https://grest.okiapps.com/',
+    url: 'https://grest.xdbsoft.com/',
     collectionName: 'notes',
     aliasName: 'alias'
   }
